e1d4bb941115f05c429bc77d3eda6acc6cd2ee4b
Sebbekai <anton.wermelin87@gmail.com>
2016-09-27 20:26:03 +0200

    Cleaning socket.html and server.js

--- server.js
+++ server.js
@@ -3,19 +3,14 @@ var url = require('url');
 var fs = require('fs');
 var io = require('socket.io');
 var express = require('express');
-
 var app = express();
-
-
+var port = 8001;
 var http = require('http').Server(app);
 
-
-http.listen(8001, function(){
-  console.log('listening on *:8001');
+http.listen(port, function(){
+  console.log('Gameserver listening on *:'+ port);
 });
-
 app.use(express.static('public'));
-
 
 var listener = io.listen(http);
 
