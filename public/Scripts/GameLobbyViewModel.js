6cbeae3e4468f4e91c1bd49c683635fe473d1353
tentakles <ollebergling@gmail.com>
2016-10-04 19:26:37 +0200

    create game implemented

--- public/Scripts/GameLobbyViewModel.js
+++ public/Scripts/GameLobbyViewModel.js
@@ -7,14 +7,23 @@ function GameLobbyViewModel(socket) {
      ***  Variabler  ***
      *******************/
 
-    self.title = ko.observable("Mitt fina namn");
-    self.showGame = ko.observable(false);
-    self.startMode = ko.observable(true);
-    self.lobbyMode = ko.observable(false);
+    self.title = ko.observable("");
+
     self.nickname = ko.observable("");
     self.chatRow = ko.observable("");
+    
+    self.createGameName = ko.observable("");
+    self.createGamePassword = ko.observable("");
+
     self.chats = ko.observableArray([]);
     self.games = ko.observableArray([]);
+
+    //modes
+    self.lobbyMode = ko.observable(false);
+    self.startMode = ko.observable(true);
+    self.lobbyMode = ko.observable(false);
+    self.gameMode = ko.observable(false);
+    self.createGameMode = ko.observable(false);
 
     /********************************************
     Initialisering
@@ -23,11 +32,17 @@ function GameLobbyViewModel(socket) {
     //initierar vymodellen
     self.init = function () {
         //self.games(["hey", "loo", "there"]);
-        self.games.push({ name: 'spel1', available: '3/4' });
-        socket.on('chat', function (data) {
+        //self.games.push({ name: 'spel1', available: '3/4' });
+        socket.on('server_chat', function (data) {
             self.chats.push(data);
             $('#chatBox').animate({ scrollTop: $('#chatBox').prop("scrollHeight") }, 100);
         });
+
+        socket.on('server_games', function (data) {
+            self.games(data);
+        });
+
+        socket.emit('client_games', {});
     }
 
     self.init();
@@ -35,13 +50,26 @@ function GameLobbyViewModel(socket) {
     self.joinGame = function (item) {
         alert('joinGame');
     };
+    
+    self.cancelCreateGame = function () {
+        self.createGameMode(false);
+        self.gameMode(true);
+    };
+
+    self.doCreateGame = function () {
+        socket.emit('client_create_game', { 'name': self.createGameName(), 'password': self.createGamePassword() });
+    };
 
     self.createGame = function () {
-        alert('createGame');
+        self.createGameName("");
+        self.createGamePassword("");
+        
+        self.createGameMode(true);
+        self.gameMode(false);
     };
 
     self.submitChat = function () {
-        socket.emit('client_data', { 'line': self.chatRow(), 'user': self.nickname() });
+        socket.emit('client_chat', { 'line': self.chatRow(), 'user': self.nickname() });
         self.chatRow("");
     };
 
@@ -53,8 +81,10 @@ function GameLobbyViewModel(socket) {
     };
 
     self.submitNickname = function () {
+        self.createGameMode(false);
         self.startMode(false);
         self.lobbyMode(true);
+        self.gameMode(true);
     }
 }
 
--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var app = express();
 var port = 8001;
 var http = require('http').Server(app);
 
+var games = [];
+//games.push({ name: 'spel1', available: '3/4',joinable:true });
+//games.push({ name: 'spel2', available: '0/4', joinable: false });
+
 http.listen(port, function () {
     console.log('Gameserver listening on *:' + port);
 });
@@ -15,9 +19,21 @@ app.use(express.static('public'));
 var listener = io.listen(http);
 
 listener.sockets.on('connection', function (socket) {
-    socket.on('client_data', function (data) {
+    socket.on('client_chat', function (data) {
         console.log(data.line);
-        listener.sockets.emit('chat', data);
+        listener.sockets.emit('server_chat', data);
     });
+
+    socket.on('client_games', function () {
+        console.log('client_games');
+        socket.emit('server_games', games);
+    });
+
+    socket.on('client_create_game', function (data) {
+        console.log('client_create_game');
+        games.push({ name: data.name,password:data.password, available: 'N/A', joinable: true });
+        listener.sockets.emit('server_games', games);
+    });
+
 });
 
