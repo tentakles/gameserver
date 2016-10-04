947d1ccadeed049f17b2cad11d2e49d7707ba152
Sebbekai <anton.wermelin87@gmail.com>
2016-10-04 18:18:05 +0200

    Better chat, keypress and maxheight

--- public/Scripts/GameLobbyViewModel.js
+++ public/Scripts/GameLobbyViewModel.js
@@ -9,34 +9,42 @@ function GameLobbyViewModel(socket) {
 
     self.title = ko.observable("Mitt fina namn");
     self.showGame = ko.observable(false);
-	self.startMode=ko.observable(true);		
-	self.lobbyMode=ko.observable(false);	
-	self.nickname=ko.observable("");
-	self.chatRow=ko.observable("");	
-	self.chats=ko.observableArray([]);
-	
+    self.startMode = ko.observable(true);
+    self.lobbyMode = ko.observable(false);
+    self.nickname = ko.observable("");
+    self.chatRow = ko.observable("");
+    self.chats = ko.observableArray([]);
+
     /********************************************
     Initialisering
     *********************************************/
 
     //initierar vymodellen
-    self.init = function () {	
-		socket.on('chat', function(data){
-			self.chats.push(data);
-		// $('#chat').animate({ scrollTop: $('#chat').prop("scrollHeight") }, 500); 
-			}); 
+    self.init = function () {
+        socket.on('chat', function (data) {
+            self.chats.push(data);
+            $('#chatBox').animate({ scrollTop: $('#chatBox').prop("scrollHeight") }, 100);
+        });
     }
 
-    self.init();	
-	
-	self.submitChat=function(){	
-		socket.emit('client_data', {'line':self.chatRow(),'user':self.nickname() });	 
-		self.chatRow("");
-		};
-	
-	self.submitNickname=function(){	
-		self.startMode(false);
-		self.lobbyMode(true);
-		}
+    self.init();
+
+    self.submitChat = function () {
+        socket.emit('client_data', { 'line': self.chatRow(), 'user': self.nickname() });
+        self.chatRow("");
+    };
+
+
+    self.submitChatKeyPress = function (d, e) {
+        if (e.keyCode == 13) {
+            self.submitChat();
+        }
+        return true;
+    };
+
+    self.submitNickname = function () {
+        self.startMode(false);
+        self.lobbyMode(true);
+    }
 }
 
